fa3f9dee50c0cd20f3725d07ecafe39282638a13
Mattis <88989926+Mattis49@users.noreply.github.com>
2023-07-10 11:43:53 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+function educationButton(){
+    document.getElementById("education-timeline").style.display="block";
+    document.getElementById("work-timeline").style.display="none";
+    document.getElementById("uil-suitcase").style.color="#0706066e";
+    document.getElementById("uil-graduation-cap").style.color="#705edc";
+}
+
+function workButton(){
+document.getElementById("education-timeline").style.display="none";
+document.getElementById("work-timeline").style.display="block";
+document.getElementById("uil-graduation-cap").style.color="#0706066e";
+document.getElementById("uil-suitcase").style.color="#705edc";
+}
