4d40b4317ce278ddb006eee814c6fcefe825a0a0
Mattisbbn <mattisbbn@gmail.com>
2024-04-11 23:03:03 +0200

    Ajustements

--- index.js
+++ index.js
@@ -26,13 +26,3 @@ function settings(){
         settingsContainer.style.display = "none"
     }
 }
-
-function openSidebar(){
-    const sidebarContainer = document.getElementById("sidebar");
-
-    if (sidebarContainer.style.display != "none"){
-        sidebarContainer.style.display = 'none'
-    }else {
-        sidebarContainer.style.display = 'flex'
-    }
-}
